6d60810b745ba73b33fd3e8686f184b077e7e067
pawanchavan1245 <pawanchavan1245@gmail.com>
2026-03-13 18:09:50 +0530

    sample testing

--- server/src/app.js
+++ server/src/app.js
@@ -4,4 +4,8 @@ const app = express();
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
+app.get('/hello',(req,res)=>{
+res.json({message:"hello world"})
+})
 export default app;
+
