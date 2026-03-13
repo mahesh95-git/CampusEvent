12e0c11ce80e0fffeb1a46d8138b3509a48bf001
Mahesh Rathod <maheshrathodd.dev@gmail.com>
2026-03-13 10:00:57 +0530

    redux toolkit set up completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
-import {Input} from "@/components/ui/input"
+import { useDispatch,useSelector } from 'react-redux'
+import { increment,decrement,incrementByAmount } from './store/slices/counter'
 export default function App() {
+  const value = useSelector((state)=>state.counter.value);
+  const dispatch = useDispatch();
+console.log(value)
   return (
     <div>
-      <Button variant="outline">Button</Button>
-      <Input  type={"number"}/>
+      <h1>{value}</h1>
+      <Button variant="outline" onClick={()=>dispatch(increment())}>increment</Button>
+      <Button variant="outline" onClick={()=>dispatch(decrement())}>increment</Button>
+      <Button variant="outline" onClick={()=>dispatch(incrementByAmount(20))}>incrementByAmount</Button>
+
+      
     </div>
   )
 }
